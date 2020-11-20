589273956aedef58fce28e76acc389bf14dc2aa5
Leo Li <lilingfeng0408@outlook.com>
2020-11-20 15:51:16 +0800

    p5732

---
+++ cpp/p5732.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int a[21][21];
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+		a[i][1]=a[i][i]=1;
+	for(int i=1;i<=n;i++)
+		for(int j=2;j<i;j++)
+			a[i][j]=a[i-1][j]+a[i-1][j-1];
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+			printf("%d ",a[i][j]);
+		printf("\n");
+	}
+}
